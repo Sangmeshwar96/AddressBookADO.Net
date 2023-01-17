efe8e1d7ed93dfc92664160779e2593fceae05ac
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-01-17 20:23:10 +0530

    [Sangmeshwar] Add: Inserting New Contact Into AddressBook With Stored Procedure Implemented

--- AddressBookADO/AddressBookADO/AddressBookOperations.cs
+++ AddressBookADO/AddressBookADO/AddressBookOperations.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,53 @@ namespace AddressBookADO
     {
         public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AddressBookSystem;Integrated Security=true;";
         SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public void InsertNewContact()
+        {
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("spInsertNewContact", this.sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                Console.Write("Give FirstName: ");
+                string firstName = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@FirstName", firstName);
+                Console.Write("Give LastName: ");
+                string lastName = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@LastName", lastName);
+                Console.Write("Give Address: ");
+                string address = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@Address", address);
+                Console.Write("Give City: ");
+                string city = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@City", city);
+                Console.Write("Give State: ");
+                string state = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@State", state);
+                Console.Write("Give Zip: ");
+                string zip = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@Zip", zip);
+                Console.Write("Give PhoneNumber: ");
+                string phoneNumber = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                Console.Write("Give Email: ");
+                string email = Console.ReadLine();
+                sqlCommand.Parameters.AddWithValue("@Email", email);
+                int effectedRows = sqlCommand.ExecuteNonQuery();
+                if (effectedRows >= 1)
+                {
+                    Console.WriteLine("-----Contact Added Successfully-----");
+                }
+                else
+                    Console.WriteLine("-----Something Went Wrong-----");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
     }
 }
